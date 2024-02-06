291eaac6d04f552b51781c65c6f23d497c99810f
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-02-06 01:47:35 -0300

    ss

---
+++ app/italian/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/footers/spa-footer";
+import Header from "../components/hearders/header";
+import Nav from "../components/navs/spa-nav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-900">
+     <Header />
+      <Nav />
+      <div className="max-w-screen-lg">{children}</div>
+      <Footer />
+    </div>
+  );
+}
