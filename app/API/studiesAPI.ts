19af1d0f8b073d3eb74ebe94da5eb33ffdec1c04
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-02-10 01:39:53 -0300

    gets

--- app/API/studiesAPI.ts
+++ app/API/studiesAPI.ts
@@ -5,7 +5,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const stu = studies();
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://dominio-de-tu-aplicacion-angular.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
