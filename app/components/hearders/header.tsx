f96df088dd845095323d9ec106de220444ed137c
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-03-04 15:27:01 -0300

    oji

--- app/components/hearders/header.tsx
+++ app/components/hearders/header.tsx
@@ -2,9 +2,9 @@ import { FaCode } from "react-icons/fa";
 
 const Header = () => {
     return (
-      <header className="w-11/12 h-36 md:h-96 flex justify-center items-center bg-sky-600 mt-20 md:mt-24 mb-12 rounded-full">
+      <header className=" w-11/12 h-36 md:h-96 flex justify-center items-center bg-sky-600 mt-20 md:mt-24 mb-12 rounded-full">
       
-          <h1 className=" w-5/6 text-3xl sm:text-5xl md:text-8xl text-white 
+          <h1 className="w-5/6 text-3xl sm:text-5xl md:text-8xl text-white 
            flex justify-around items-center"> <FaCode />
             <b>
              
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,40 @@
+'use client'
 import Link from "next/link"
+import { useEffect } from "react";
 
 export default function Page() {
+  useEffect(() => {
+    const rainbowElement = document.getElementById('arc');
+    if (rainbowElement) {
+      rainbowElement.classList.add('anim');
+
+      rainbowElement.addEventListener('animationend', () => {
+        rainbowElement.classList.remove('anim');
+      });
+      return () => {
+        rainbowElement.removeEventListener('animationend', () => {
+          rainbowElement.classList.remove('anim');
+        });
+      };
+    }
+    const rainbowElements = document.querySelectorAll('.arc-bd');
+    rainbowElements.forEach(element => {
+      element.classList.add('animbd');
+      element.addEventListener('animationend', () => {
+        element.classList.remove('animbd');
+      });
+    });
+  }, []);
   return (
     <main className="flex-1 h-full flex bg-black pb-48">
       <div className="w-full h-full flex justify-center content-center flex-col p-9">
         <b className='flex justify-center'>
-          <h1 className="text-center p-8 mt-20 mb-16 bg-sky-600 rounded-full text-3xl md:text-5xl w-full text-white">
+          <h1 id="arc" className="text-center p-8 mt-20 mb-16 bg-sky-600 rounded-full text-3xl md:text-5xl w-full text-white">
             Hi! I`m Francisco Hernandorena <i className="fa-solid fa-code"></i>
           </h1>
         </b>
         <div className="flex flex-1 flex-wrap justify-center">
-          <div className=" my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className="arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
             <h3 className='text-3xl p-1 text-center mb-3' >
               Bienvenido!
               <br /> este es mi sitio oficial
@@ -28,7 +52,7 @@ export default function Page() {
               Vamos! <i className="fa-solid fa-bolt"></i>
             </Link></div>
           </div>
-          <div className="  my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className=" arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
             <h3 className='text-3xl p-1 text-center mb-3' >
             Welcome!
               <br /> this is my oficial site
@@ -46,7 +70,7 @@ export default function Page() {
               Come on!  <i className="fa-solid fa-bolt"></i>
             </Link></div>
           </div>
-          <div className=" my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className="arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
             <h3 className='text-3xl p-1 text-center mb-3' >
             Benvenuto!
               <br /> questo è il mio sito ufficiale
