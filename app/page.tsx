c6cc9339ad5ab31fc129aca2aa76f1ba2c09bad6
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-03-04 23:08:42 -0300

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,27 @@
 'use client'
 import Link from "next/link"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    const rainbowElement = document.getElementById('arc');
-    if (rainbowElement) {
-      rainbowElement.classList.add('anim');
+  const [loaded, setLoaded] = useState(false);
 
-      rainbowElement.addEventListener('animationend', () => {
-        rainbowElement.classList.remove('anim');
-      });
-      return () => {
-        rainbowElement.removeEventListener('animationend', () => {
-          rainbowElement.classList.remove('anim');
-        });
-      };
-    }
-    const rainbowElements = document.querySelectorAll('.arc-bd');
-    rainbowElements.forEach(element => {
-      element.classList.add('animbd');
-      element.addEventListener('animationend', () => {
-        element.classList.remove('animbd');
-      });
-    });
-  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []); 
   return (
     <main className="flex-1 h-full flex bg-black pb-48">
       <div className="w-full h-full flex justify-center content-center flex-col p-9">
         <b className='flex justify-center'>
-          <h1 id="arc" className="text-center p-8 mt-20 mb-16 bg-sky-600 rounded-full text-3xl md:text-5xl w-full text-white">
+          <h1 className={`my-tit text-center p-8 mt-20 mb-16 bg-sky-600 rounded-full text-3xl md:text-5xl w-full text-white ${loaded ? 'tit-loaded' : ''}`}>
             Hi! I`m Francisco Hernandorena <i className="fa-solid fa-code"></i>
           </h1>
         </b>
         <div className="flex flex-1 flex-wrap justify-center">
-          <div className="arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className={`my-component my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl ${loaded ? 'loaded' : ''}`}>
             <h3 className='text-3xl p-1 text-center mb-3' >
               Bienvenido!
               <br /> este es mi sitio oficial
@@ -52,7 +39,7 @@ export default function Page() {
               Vamos! <i className="fa-solid fa-bolt"></i>
             </Link></div>
           </div>
-          <div className=" arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className={`my-component my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl ${loaded ? 'loaded' : ''}`}>
             <h3 className='text-3xl p-1 text-center mb-3' >
             Welcome!
               <br /> this is my oficial site
@@ -70,7 +57,7 @@ export default function Page() {
               Come on!  <i className="fa-solid fa-bolt"></i>
             </Link></div>
           </div>
-          <div className="arc-bd my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl">
+          <div className={`my-component my-3 bg-gray-900 transition-linear duration-300 text-white w-full py-5 flex items-center md:justify-around rounded-xl font-medium border-2 border-solid border-sky-600  md:m-5 md:py-30 text-xl flex-col md:w-72 md:h-72 md:rounded-3xl ${loaded ? 'loaded' : ''}`}>
             <h3 className='text-3xl p-1 text-center mb-3' >
             Benvenuto!
               <br /> questo è il mio sito ufficiale
