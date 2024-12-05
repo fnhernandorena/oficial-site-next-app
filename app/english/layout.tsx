7bc2196e2a11adfb9c21edd0e895b31023dbb7af
fnhrnandorena <fnhernandorena@gmail.com>
2024-12-05 13:39:10 -0300

    s

--- app/english/layout.tsx
+++ app/english/layout.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 import Header from "../components/hearders/header";
 import Nav from "../components/navs/eng-nav";
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   }, []);
   return (
     <div className="flex flex-col items-center justify-center bg-gray-900">
-     <Header />
+      <Header />
       <div className="max-w-screen-lg w-full pb-32">{children}</div>
       <Nav />
     </div>
